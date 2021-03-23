c54621ec5800e79f8bb7c6515f9d014f4899dca2
Amr Wael <amr.wael.dev@gmail.com>
2021-03-23 22:14:24 +0200

    Add event listeners and helper functions

--- js/app.js
+++ js/app.js
@@ -7,3 +7,31 @@ for (let i = 1; i <= count; i++) {
 
     ul.insertAdjacentHTML("beforeEnd", htmlTextToAdd);
 }
+
+window.onscroll = function() {showButton()};
+button.onclick = function() {scrollToSection(0)};
+
+function scrollToSection(id) {
+    if (id === 0) {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    } else {
+        const elem = document.getElementById("section" + id);
+    
+        elem.scrollIntoView({
+            block: "start",
+            behavior: "smooth",
+            inline: "start"
+        });
+    }
+}
+
+function showButton() {
+    if (document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
