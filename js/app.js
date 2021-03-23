01c2fc93d466f7b56a8d61cb95542722b5122608
Amr Wael <amr.wael.dev@gmail.com>
2021-03-23 22:11:22 +0200

    Define global variables

---
+++ js/app.js
@@ -0,0 +1,3 @@
+let count = document.getElementsByTagName("main")[0].childElementCount - 2;
+const ul = document.getElementById("navbar__list");
+const button = document.getElementById("scrollToTop");
