c8fd0a840c0a62969111d2eabbcd22f6de71dca6
Amr Wael <amr.wael.dev@gmail.com>
2021-03-23 22:12:54 +0200

    Add navigation links dynamically depending on the number of sections

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 let count = document.getElementsByTagName("main")[0].childElementCount - 2;
 const ul = document.getElementById("navbar__list");
 const button = document.getElementById("scrollToTop");
+
+for (let i = 1; i <= count; i++) {
+    let htmlTextToAdd = '<li><a class="menu__link" onclick="scrollToSection(' + i + ')">Section ' + i + '</a></li>';
+
+    ul.insertAdjacentHTML("beforeEnd", htmlTextToAdd);
+}
